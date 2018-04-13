9cdc93536329fe139a5fd6f8870ec4e7011a8e0f
yusufrizalh <yusufrizalh@gmail.com>
2018-04-13 11:22:30 +0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,8 +26,12 @@ export class HomePage {
     this.geolocation.getCurrentPosition()
       .then((position) => {               // resolve
         // posisi latitude dan longitude
-        let latLng = new google.maps.LatLng();
-
+        let latLng = new google.maps.LatLng(
+          //position.coords.latitude,
+          //position.coords.longitude
+          -6.893426, 107.602941
+        );
+        
         // option map yang dibuka
         let mapOptions = {
           center: latLng,
@@ -42,5 +46,27 @@ export class HomePage {
       }, (error) => {                 // reject
         console.log(error);
       });
+  }
+
+  // membuat marker pada map
+  addMarker(){
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h3>Information!</h3>";
+    this.addInfoWindow(marker, content);
+  }
+
+  // buat info window
+  addInfoWindow(marker, content){
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
   }
 }
